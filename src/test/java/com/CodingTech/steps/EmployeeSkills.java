fb99f369c866a08936895efb02c2cb0d0dfff4fc
ktemiz <online4kzm@gmail.com>
2020-06-14 15:49:55 -0400

    add features folder

---
+++ src/test/java/com/CodingTech/steps/EmployeeSkills.java
@@ -0,0 +1,11 @@
+package com.CodingTech.steps;
+
+import org.hamcrest.core.CombinableMatcher;
+
+import com.CodingTech.utils.CommonMethods;
+
+public class EmployeeSkills extends CommonMethods {
+	
+	
+
+}
